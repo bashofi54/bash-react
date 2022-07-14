d6402ee2695f89e14522b059ba0225e2f4bb7a43
bashofi54 <bashofi54@gmail.com>
2022-07-14 15:05:01 +0700

    fixs problem autoprefixer4

--- src/Materi/Styling/Bootstrap/index.jsx
+++ src/Materi/Styling/Bootstrap/index.jsx
@@ -7,7 +7,7 @@ export default class Bootstrap extends React.Component {
         return (
             <Navbar bg="primary" variant="dark">
             <Container>
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Navbar.Brand href="#home">Eduwork</Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
                     <Nav.Link href="#features">Features</Nav.Link>
