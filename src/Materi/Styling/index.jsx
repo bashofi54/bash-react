adb471a2cc61edf33d1ffed377190f0d4aa51de8
bashofi54 <bashofi54@gmail.com>
2022-07-16 14:25:48 +0700

    Style component

--- src/Materi/Styling/index.jsx
+++ src/Materi/Styling/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import Bootstrap from "./Bootstrap";
+import Bootstrap from "./Bootstrap";
 import Inline from "./Inline";
 import Module from "./Module";
 //import Plain from "./Plain";
@@ -16,7 +16,7 @@ export default class Styling extends React.Component {
                 <Inline />
                 <Module />
                 <Style />
-                {/* <Bootstrap /> */}
+                <Bootstrap />
             </div>
         )
     }
